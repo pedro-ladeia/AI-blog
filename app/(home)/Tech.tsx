5bb02ef58e670da80e24d15da7d446ddcfe2999c
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-11-06 17:54:24 -0300

    :construction: Created tthe grid

---
+++ app/(home)/Tech.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import TitleBox from '../(shared)/TitleBox'
+
+type TechCardProps = { 
+    title: string,
+    description: string,
+    image: string,
+    author: string,
+    date: string
+}
+
+
+/*const TechCard = ({title, description, image, author, date}: TechCardProps) => {
+    return(
+        <div className=''>
+
+        </div>
+    )
+}*/
+
+
+type Props = {}
+
+const Tech = (props: Props) => {
+  return (
+    <section>
+        <hr className='border-1' />
+        <div className='flex items-center gap-3 my-8'>
+            <TitleBox
+                category='HOT'
+                description='THE MAIN NEWS OF IA WORLD'
+                textColor='text-wh-900'
+            />
+        </div>
+        <div className='sm:grid grid-cols-2 grid-rows-3 gap-8 my-5 h-96'>
+            <div className='col-span-1 row-span-3 bg-wh-300'></div>
+            <div className='col-span-1 row-span-1 bg-wh-300'></div>
+            <div className='col-span-1 row-span-1 bg-wh-300'></div>
+            <div className='col-span-1 row-span-1 bg-wh-300'></div>
+        </div>
+    </section>
+  )
+}
+
+export default Tech
