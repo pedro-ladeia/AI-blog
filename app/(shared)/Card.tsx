50a7a3582b0b33024f0312b7100bdca7854d59f3
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-11-09 23:52:04 -0300

    :construction: Made the card structure

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {
@@ -10,7 +11,35 @@ type Props = {
 
 const Card = ({className, imageHeight, isSmall = false, isLongForm = false}: Props) => {
   return (
-    <div>Card</div>
+    <div className={className}>
+      <Link 
+        className='basis-full hover:opacity-70'
+        href='/'
+        >
+          <div className={`relative w-auto ${imageHeight}`}>
+            image
+          </div>
+      </Link>
+
+      <div className='basis-full'>
+        <Link href='/'>
+            <h4 className={
+              `font-bold hover:text-accent-green ${isSmall? 'text-base' : 'text-lg'} 
+              ${isSmall? 'line-clamp-2' : ''}`}
+              >
+              Title
+            </h4>
+        </Link>
+
+        <div className={`${isSmall? 'my-5' : 'flex my-3'} gap-3`}>
+          <h5 className='font-semibold text-xs '>author</h5>
+          <h6 className='text-wh-300 text-xs'>date</h6>
+        </div>
+
+        <p className={`text-wh-500 ${isLongForm? 'line-clamp-5' : 'line-clamp-3'}`}>Long text</p>
+      </div>
+
+    </div>
   )
 }
 
