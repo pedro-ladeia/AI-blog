4587f10978506bd289a31ab0eb67f24eed0d4992
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-04 22:17:43 -0300

    :bug: fix: Fixed responsiveness with cards

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -3,48 +3,24 @@ import TitleBox from '../(shared)/TitleBox'
 import Card from '../(shared)/Card'
 
 
+
 type Props = {}
 
 const Tech = (props: Props) => {
   return (
     <section>
-        <hr className='border-1' />
-        <div className='flex items-center gap-3 my-8'>
-            <TitleBox
-                category='HOT'
-                description='THE MAIN NEWS OF IA WORLD'
-                textColor='text-wh-900'
-            />
-        </div>
-        <div className='sm:grid grid-cols-2 grid-rows-3 gap-8 my-5 h-96'>
-            <Card
-              className='bg-wh-500 col-span-1 row-span-3'
-              imageHeight='h-96'
-              isLongForm
-
-            />
-            {/* SMALL CARDS */}
-            <Card
-              className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
-              imageHeight='h-48'
-              isSmall
-
-            />
-
-            <Card
-              className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
-              imageHeight='h-48'
-              isSmall
-
-            />
-
-            <Card
-              className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
-              imageHeight='h-48'
-              isSmall
-
-            />
-        </div>
+      <hr className='broder-1 mb-8'/>
+      <TitleBox
+        category='HOT'
+        description='THE MAIN NEWS IN THE IA WORLD'
+        textColor='text-wh-900'
+      />
+      <div className=' sm:grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-8 my-5'>
+        <Card className='bg-wh-500 col-span-1 row-span-3' imageHeight='h-96' isLongText/>
+        <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3' imageHeight='h-48' />
+        <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3' imageHeight='h-48' />
+        <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3' imageHeight='h-48' />
+      </div>
     </section>
   )
 }
