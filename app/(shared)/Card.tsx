2b8ea8a9d860341a9a2e7d55e870bdf8638f748c
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-04 22:16:19 -0300

    :bug: fix: Fixed responsiveness

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,46 +1,35 @@
-import Link from 'next/link';
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {
-    className?: string;
-    imageHeight?: string;
-    isSmall?: boolean;
-    isLongForm?: boolean;
-
+  className?: string,
+  imageHeight: string,
+  isSmall?: boolean,
+  isLongText?: boolean
 }
 
-const Card = ({className, imageHeight, isSmall = false, isLongForm = false}: Props) => {
-  return (
+const Card = ({className, imageHeight, isSmall = false, isLongText = false}:Props) => {
+  return(
     <div className={className}>
-      <Link 
-        className='basis-full hover:opacity-70'
-        href='/'
-        >
-          <div className={`relative w-auto ${imageHeight}`}>
-            image
-          </div>
+      <Link className='basis-full hover:opacity-70' href= '/'>
+        <div className={`relative w-auto mb-3 ${imageHeight}`}>image</div>
       </Link>
-
       <div className='basis-full'>
-        <Link href='/'>
-            <h4 className={
-              `font-bold hover:text-accent-green ${isSmall? 'text-base' : 'text-lg'} 
-              ${isSmall? 'line-clamp-2' : ''}`}
-              >
-              Title
-            </h4>
+        <Link className='basis-full hover:opacity-70' href= '/'>
+          <h5 className ={`font-bold hover:text-accent-green
+            ${isSmall? 'text-base' : 'text-lg'}
+            ${isSmall? 'line-clamp-2' : ''}
+          `}>Title</h5>
         </Link>
-
-        <div className={`${isSmall? 'my-5' : 'flex my-3'} gap-3`}>
-          <h5 className='font-semibold text-xs '>author</h5>
-          <h6 className='text-wh-300 text-xs'>date</h6>
+        <div className={`${isSmall? 'my-2' : 'flex my-3'}`}>
+          <h5 className='font-semibold text-xs'>Author</h5>
+          <h6 className='text-wh-300 text-xs'>Date</h6>
         </div>
-
-        <p className={`text-wh-500 ${isLongForm? 'line-clamp-5' : 'line-clamp-3'}`}>Long text</p>
+        <p className={`text-wh-300 ${isLongText? 'line-clamp-5' : 'line-clamp-3'}`}>
+          Snippet
+        </p>
       </div>
-
     </div>
   )
 }
-
 export default Card
