2c7c472446f37ddc4fd09634ecf354d32f8d931c
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-12 11:50:25 -0300

    :sparkles: feat: Added className like prop

--- app/(shared)/TitleBox.tsx
+++ app/(shared)/TitleBox.tsx
@@ -5,13 +5,14 @@ type Props = {
     description:string,
     backgColor?:string,
     categoryColor?:string,
-    textColor?:string
+    textColor?:string,
+    className?:string
 }
 
-const TitleBox = ({category, description, backgColor = 'bg-accent-orange', categoryColor = 'wh-900', textColor = 'text-wh-100'}: Props) => {
+const TitleBox = ({category, description, backgColor = 'bg-accent-orange', categoryColor = 'wh-900', textColor = 'text-wh-100', className}: Props) => {
   return (
     <div className='flex items-center gap-4'>
-        <h1 className={`py-2 px-6 font-bold ${backgColor} ${categoryColor}`}>{category}</h1>
+        <h1 className={`py-2 px-6 font-bold ${backgColor} ${categoryColor} ${className}`}>{category}</h1>
         <p className={`${textColor} text-xl font-bold`}>{description}</p>
     </div>
   )
