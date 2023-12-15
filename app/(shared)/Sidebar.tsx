20e52d2e10a2f83dafd5561253b4492bd4737bf8
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-14 22:47:09 -0300

    :construction: feat: Added images and a p, less the Titlebox component

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -8,18 +8,25 @@ type Props = {}
 function Sidebar({}: Props) {
   return (
     <section className=''>
-        <TitleBox
-            category='Subscribe and Follow' 
-            description='' 
-            backgColor='bg-wh-900' 
-            categoryColor='text-wh-10'
-            className='w-full text-center'
-            />
+        <h4 className='bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center'>
+          SUBSCRIBE AND FOLLOW
+        </h4>
         
         <div className='my-5 mx-5'>
             <SocialMedia isDark />
         </div>
         <Subscribe />
+        <div className='bg-wh-900 '>advert image</div>
+        <h2 className='bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center'>
+          ABOUT THE BLOG
+        </h2>
+        <div className='bg-wh-900 '>profile image</div>
+        <h2 className='py-3 px-5 text-wh-500 font-bold text-center'>
+          Alan Turing
+        </h2>
+          <p className='text-wh-500 text-center text-sm'>
+          Alan Turing was an English mathematician, father of computing science, logician, cryptanalyst.
+        </p>
     </section>
   )
 }
